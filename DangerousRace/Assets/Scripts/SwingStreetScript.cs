b7215d12e74cf61f38c5e4c4fe78f142b4e17fa3
boguwyrw <b.wyrwal@wp.pl>
2019-10-04 22:35:39 +0200

    All race track elements were added; Script for SwingStreet was made

---
+++ DangerousRace/Assets/Scripts/SwingStreetScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingStreetScript : MonoBehaviour {
+
+    private float rotationSpeed = 10.0f;
+    private bool maximumDeviation = false;
+
+
+    void Start () {
+		
+	}
+
+	void FixedUpdate () {
+
+        if (transform.localEulerAngles.z > 40.0f) {
+            maximumDeviation = true;
+        }
+        
+        if (transform.localEulerAngles.z > 355.0f) {
+            maximumDeviation = false;
+        }
+        
+        if (maximumDeviation == false) {
+            transform.Rotate(0.0f, 0.0f, rotationSpeed * Time.deltaTime);
+        }
+  
+        if (maximumDeviation == true) {
+            transform.Rotate(0.0f, 0.0f, -rotationSpeed * Time.deltaTime);
+        }
+        
+	}
+}
