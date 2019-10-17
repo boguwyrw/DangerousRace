7d63315e55b99a0a7a76ce3ac14fa1f31a2f4419
boguwyrw <b.wyrwal@wp.pl>
2019-10-17 21:53:43 +0200

    Second camera as rear view mirror was added

---
+++ DangerousRace/Assets/Scripts/CamerasScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerasScript : MonoBehaviour {
+
+    private Camera playerCamera;
+    public Camera backCamera;
+
+	void Start () {
+        playerCamera = Camera.main;
+        playerCamera.enabled = true;
+        backCamera.enabled = false;
+
+
+    }
+
+	void FixedUpdate () {
+        backCamera.enabled = true;
+        backCamera.rect = new Rect(0.175f, 0.8f, 0.65f, 0.2f);
+	}
+}
