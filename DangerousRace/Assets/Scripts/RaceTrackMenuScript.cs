d2b0dce0debc151f8a768d4ba722e2e5f0c5d68a
boguwyrw <b.wyrwal@wp.pl>
2019-12-29 12:22:26 +0100

    Small changes

--- DangerousRace/Assets/Scripts/RaceTrackMenuScript.cs
+++ DangerousRace/Assets/Scripts/RaceTrackMenuScript.cs
@@ -9,12 +9,32 @@ public class RaceTrackMenuScript : MonoBehaviour {
     public GameObject playerOne;
     public GameObject playerTwo;
     //public GameObject camera;
+    public GameObject buttons;
+
+    private bool isPlaying = false;
+    private float shotDownTime = 5;
+
+    void Start()
+    {
+        buttons.SetActive(true);
+    }
+
+    void Update()
+    {
+        ShotDownButtons();
+        if (isPlaying)
+        {
+            buttons.SetActive(false);
+        }
+        
+    }
 
     public void PlaySinglePlayerGame()
     {
         playerOne.SetActive(true);
         playerTwo.SetActive(false);
         //camera.SetActive(false);
+        //ShotDownButtons();
     }
 
     public void PlayMultiPlayerGame()
@@ -22,11 +42,21 @@ public class RaceTrackMenuScript : MonoBehaviour {
         playerOne.SetActive(true);
         playerTwo.SetActive(true);
         //camera.SetActive(false);
+        //ShotDownButtons();
     }
 
     public void BackGame()
     {
         SceneManager.LoadScene("GameMenu");
+    }
+
+    void ShotDownButtons()
+    {
+        shotDownTime -= Time.deltaTime;
+        if (shotDownTime <= 0.0f)
+        {
+            isPlaying = true;
+        }
     }
 
 }
